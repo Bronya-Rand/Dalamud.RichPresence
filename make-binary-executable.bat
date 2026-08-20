05bee47b5e4cacb04e83781c5b1d53e8a591bc4d
Bronya-Rand <40864681+Bronya-Rand@users.noreply.github.com>
2026-08-19 23:31:24 -0500

    chore: cleanup old bridge code

--- make-binary-executable.bat
+++
@@ -1,2 +0,0 @@
-git update-index --chmod=+x .\Dalamud.RichPresence\Resources\binaries\linux-x64\RichPresenceBridge
-git update-index --chmod=+x .\Dalamud.RichPresence\Resources\binaries\osx-arm64\RichPresenceBridge
